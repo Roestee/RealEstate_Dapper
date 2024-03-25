23aa79fdee91ac3dbaf660815005f6988c8aae42
İbrahim Çınğı <103828984+Roestee@users.noreply.github.com>
2024-03-25 16:35:58 +0300

    Who we are api update

---
+++ RealEstate_Dapper_Api/Controllers/WhoWeAreDetailController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+using RealEstate_Dapper_Api.Dtos.WhoWeAreDetailDtos;
+using RealEstate_Dapper_Api.Repositories.WhoWeAreRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WhoWeAreDetailController : ControllerBase
+    {
+
+        private readonly IWhoWeAreDetailRepository _whoWeAreDetailDetailRepository;
+
+        public WhoWeAreDetailController(IWhoWeAreDetailRepository whoWeAreDetailDetailRepository)
+        {
+            _whoWeAreDetailDetailRepository = whoWeAreDetailDetailRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> WhoWeAreDetailList()
+        {
+            var value = await _whoWeAreDetailDetailRepository.GetAllWhoWeAreDetailAsync();
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateWhoWeAreDetail(CreateWhoWeAreDetailDto createWhoWeAreDetailDto)
+        {
+            _whoWeAreDetailDetailRepository.CreateWhoWeAreDetail(createWhoWeAreDetailDto);
+            return Ok("Biz Kimiz Ayrıntısı Başarılı Bir Şekilde Eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteWhoWeAreDetail(int id)
+        {
+            _whoWeAreDetailDetailRepository.DeleteWhoWeAreDetail(id);
+            return Ok($"id= {id} Başarılı Bir Şekilde Silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateWhoWeAreDetail(UpdateWhoWeAreDetailDto updateWhoWeAreDetailDto)
+        {
+            _whoWeAreDetailDetailRepository.UpdateWhoWeAreDetail(updateWhoWeAreDetailDto);
+            return Ok("Biz Kimiz Ayrıntısı Başarılı Bir Şekilde Güncellendi");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetWhoWeAreDetail(int id)
+        {
+            var value = await _whoWeAreDetailDetailRepository.GetWhoWeAreDetailAsync(id);
+            return Ok(value);
+        }
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/WhoWeAreRepository/IWhoWeAreDetailRepository.cs
@@ -0,0 +1,14 @@
+﻿using RealEstate_Dapper_Api.Dtos.WhoWeAreDetailDtos;
+using RealEstate_Dapper_Api.Dtos.WhoWeAreDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.WhoWeAreRepository
+{
+    public interface IWhoWeAreDetailRepository
+    {
+        Task<List<ResultWhoWeAreDetailDto>> GetAllWhoWeAreDetailAsync();
+        void CreateWhoWeAreDetail(CreateWhoWeAreDetailDto createWhoWeAreDetailDto);
+        void DeleteWhoWeAreDetail(int id);
+        void UpdateWhoWeAreDetail(UpdateWhoWeAreDetailDto updateWhoWeAreDetailDto);
+        Task<GetByIdWhoWeAreDetalDto> GetWhoWeAreDetailAsync(int id);
+    }
+}
