004d286c0a466779683eb09fa5fe2c4fa6d28cbc
İbrahim Çınğı <103828984+Roestee@users.noreply.github.com>
2024-03-27 15:20:44 +0300

    who we are detail api consume

--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
@@ -1,11 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.WhoWeAreDtos;
 
 namespace RealEstate_Dapper_UI.ViewComponents.HomePage
 {
     public class _DefaultWhoWeAreComponentPartial: ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultWhoWeAreComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44315/api/WhoWeAreDetail");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<List<ResultWhoWeAreDetailDto>>(jsonData);
+                var value = data.FirstOrDefault();
+                if (value != null)
+                {
+                    ViewBag.title = value.Title;
+                    ViewBag.subTitle = value.SubTitle;
+                    ViewBag.description1 = value.Description1;
+                    ViewBag.description2 = value.Description2;
+                }
+
+                return View();
+            }
+
             return View();
         }
     }
