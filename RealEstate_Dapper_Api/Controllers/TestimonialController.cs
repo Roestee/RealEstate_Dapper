d7b7a9f11f206658426b0544a85db1afb34b7bdb
İbrahim Çınğı <103828984+Roestee@users.noreply.github.com>
2024-03-28 21:16:11 +0300

    admin panel update

---
+++ RealEstate_Dapper_Api/Controllers/TestimonialController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Dtos.TestimonialDtos;
+using RealEstate_Dapper_Api.Repositories.TestimonialRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialController : ControllerBase
+    {
+        private readonly ITestimonialRepository _testimonialRepository;
+
+        public TestimonialController(ITestimonialRepository testimonialRepository)
+        {
+            _testimonialRepository = testimonialRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTestimonialList()
+        {
+            var value = await _testimonialRepository.GetAllTestimonialAsync();
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTestimonial(CreateTestimonialDto createTestimonialDto)
+        {
+            _testimonialRepository.CreateTestimonial(createTestimonialDto);
+            return Ok("Referans Başarılı Bir Şekilde Eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteTestimonial(int id)
+        {
+            _testimonialRepository.DeleteTestimonial(id);
+            return Ok($"id= {id} Başarılı Bir Şekilde Silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateTestimonial(UpdateTestimonialDto updateTestimonialDto)
+        {
+            _testimonialRepository.UpdateTestimonial(updateTestimonialDto);
+            return Ok("Referans Başarılı Bir Şekilde Güncellendi");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTestimonial(int id)
+        {
+            var value = await _testimonialRepository.GetTestimonial(id);
+            return Ok(value);
+        }
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/TestimonialRepository/TestimonialRepository.cs
@@ -0,0 +1,81 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.TestimonialDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.TestimonialRepository
+{
+    public class TestimonialRepository: ITestimonialRepository
+    {
+        private readonly Context _context;
+
+        public TestimonialRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ResultTestimonialDto>> GetAllTestimonialAsync()
+        {
+            var query = "Select * From Testimonial";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultTestimonialDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async void CreateTestimonial(CreateTestimonialDto testimonialDto)
+        {
+            var query = "insert into Testimonial (NameSurname, Title, Comment) values (@nameSurname, @title, @comment)";
+            var parameters = new DynamicParameters();
+            parameters.Add("@nameSurname", testimonialDto.NameSurname);
+            parameters.Add("@title", testimonialDto.Title);
+            parameters.Add("@comment", testimonialDto.Comment);
+
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async void DeleteTestimonial(int id)
+        {
+            var query = "Delete from Testimonial Where TestimonialId =@testimonialId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@testimonialId", id);
+
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async void UpdateTestimonial(UpdateTestimonialDto updateTestimonialDto)
+        {
+            var query =
+                "Update Testimonial Set NameSurname=@nameSurname, Title=@title, Comment=@comment where TestimonialId=@testimonialId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@nameSurname", updateTestimonialDto.NameSurname);
+            parameters.Add("@title", updateTestimonialDto.Title);
+            parameters.Add("@comment", updateTestimonialDto.Comment);
+            parameters.Add("@testimonialId", updateTestimonialDto.TestimonialId);
+            
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async Task<ResultTestimonialDto> GetTestimonial(int id)
+        {
+            var query = "Select * From Testimonial Where TestimonialId=@testimonialId";
+            var parameters = new DynamicParameters();
+            parameters.Add("testimonialId", id);
+
+            using (var connection = _context.CreateConnection())
+            {
+                var value = await connection.QueryFirstOrDefaultAsync<ResultTestimonialDto>(query, parameters);
+                return value;
+            }
+        }
+    }
+}
